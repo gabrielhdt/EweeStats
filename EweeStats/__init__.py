356c784ed257a0abcc212bfe2cce8d078da9719e
gabrielhdt <gabrielhondet@gmail.com>
2014-05-01 11:12:29 +0200

    Ajouté test des dépendances dans __init__.py

--- EweeStats/__init__.py
+++ EweeStats/__init__.py
@@ -5,16 +5,25 @@
     ~~~~~~~~~
     
     :copyright: Copyright 2014 Gabriel Hondet <gabrielhondet@gmail.com>
-    :license: Apache 2.0, see LICENSE for details
+    :license: GPL-3.0, see LICENSE for details
 """
 
 import os
 import sys
+import imp
 
 __version__ = '0.0'
 
 #Check python version
 if sys.version_info[:2] < (2, 7):
     raise ImportError('EweeStats requires Python version 2.7 or above')
-
-print('Init done')
+    
+try:
+    imp.find_module('smbus')
+    imp.find_module('ezodf2')
+    imp.find_module('pyfirmata')
+    imp.find_module('pygal')
+except ImportError:
+    print('EweeStats needs smbus-cffi, ezodf2, pyfirmata and pygal\n')
+    print('Install it with :\n \teasy_install-2.7 <package-name>')
+    sys.exit()
