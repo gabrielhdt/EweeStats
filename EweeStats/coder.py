413395414a3f6d8c0a4383237139967bfad0a92a
gabrielhdt <gabrielhondet@gmail.com>
2014-05-18 00:47:25 +0000

    Update coder.py
    completely rewritten coder.py, speed km/h, one value each 0.25 seconds

--- EweeStats/coder.py
+++ EweeStats/coder.py
@@ -2,66 +2,68 @@
 # -*- coding: utf-8 -*-
 #
 
-import pyfirmata
 import os
 import sys
 import shlex
 import re
 import time
+import RPi.GPIO as GPIO
+
+last_encoded = 0
+encoder_value = 0
 
 def coder():
-    board = pyfirmata.Arduino('/dev/ttyACM1')
-    coder_pin_1 = 2
-    coder_pin_2 = 3
-    lastMSB = 0
-    lastLSB = 0
-    speed = 0.
-    circonference = 0.62
-    last_op = 0
-    encoder_value = 0
+    encoder_pin_1 = 23
+    encoder_pin_2 = 24
+    
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    temps = 0
+    vitesse = 0
+    last_encoded = 0
     MSB = 0
     LSB = 0
-    last_encoded = 0
+    circonference = 0.6283
 
-    print(type(MSB))
-    print(type(LSB))
+    global encoder_value
+    GPIO.add_event_detect(23, GPIO.RISING, callback=update_encoder)
+    GPIO.add_event_detect(24, GPIO.RISING, callback=update_encoder)
+    temps = time.time()
 
-    board.digital[coder_pin_1].write(1)
-    board.digital[coder_pin_2].write(1)
-    pin1 = board.get_pin('d:3:i')
-    pin2 = board.get_pin('d:2:i')
+
 
     while True:
         try:
-            compteur = time.time() - last_op
-            if compteur >= 1:
-                speed = encoder_value*circonference/96
+            compteur = (time.time() - temps)
+
+            if compteur >= 0.25:
+                vitesse = 4*encoder_value * 0.22222222*circonference/96
                 encoder_value = 0
-                last_op = time.time()
-                print('Speed : {s}'.format(s = speed))
+                temps = time.time()
 
-#            MSB = board.digital[2].read()
-#            LSB = board.digital[3].read()
-            MSB = pin1.read()
-            LSB = pin2.read()
-            print('MSB : {m} et LSB : {l}'.format(m = MSB, l = LSB))
-
-            if  (MSB != lastMSB) or (LSB != lastLSB):
-                encoded = (MSB << 1) |LSB
-                sum_coded = (last_encoded << 2) | encoded
-                if (sum == 0b1101 or sum == 0b0100 or sum == 0b0010 or sum == 0b1011):
-                    encoder_value += 1
-
-                if (sum == 0b1110 or sum == 0b0111 or sum == 0b0001 or sum == 0b1000):
-                    encoder_value -=1
-
-                last_encoded = encoded
-                lastMSB = MSB
-                lastLSB = LSB
+                print(round(3.6*vitesse, 4))
 
         except KeyboardInterrupt:
-            board.exit()
             sys.exit()
+
+def update_encoder(channel):
+    global encoder_value
+    global last_encoded
+    MSB = GPIO.input(23)
+    LSB = GPIO.input(24)
+
+    encoded = (MSB << 1) |LSB
+    sum = (last_encoded << 2) | encoded
+
+    if (sum == 0b1101 or sum == 0b0100 or sum == 0b0010 or sum == 0b1011):
+        encoder_value +=1
+    if (sum == 0b1110 or sum == 0b0111 or sum == 0b0001 or sum == 0b1000):
+        encoder_value -= 1
+
+    last_encoded = encoded
+    return 0
 
 
 if __name__ == '__main__':
