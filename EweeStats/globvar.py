19720340de893a99321854be8796cbe60ea7aa14
gabrielhdt <gabrielhondet@gmail.com>
2014-05-20 20:58:54 +0200

    Update AnalogGraphThread, main, coder, readconfig, set_dir_dev
    added globvar containing var for coder
    setup coder in a new thread
    added setup of GPIO in set_dir_dev
    new element in config tuple : coders pins
    readconfig reads pins of the coder

---
+++ EweeStats/globvar.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+#  globvar.py
+#  
+#  Copyright 2014 Gabriel Hondet <gabrielhondet@gmail.com>
+#
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#
+
+last_encoded = 0
+encoder_value = 0
